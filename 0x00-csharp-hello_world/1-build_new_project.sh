1e20bf800c0bd1e9eb651543490f1bede13283d0
Christopher-Caswell <christopher.caswell@rocketmail.com>
2021-07-12 17:22:10 -0500

    No comments, still working code

--- 0x00-csharp-hello_world/1-build_new_project.sh
+++ 0x00-csharp-hello_world/1-build_new_project.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
-# Write a Bash script that initializes and builds
-# a new C# project inside a folder titled 1-new_project
-
 mkdir 1-new_project
 cd 1-new_project
 dotnet new console
