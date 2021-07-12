5fe2ef28e10addbb7847a72c109c561438725b07
Christopher-Caswell <christopher.caswell@rocketmail.com>
2021-07-12 17:09:37 -0500

    Init 1, reup 0 typo

--- 0x00-csharp-hello_world/0-initialize_new_project.sh
+++ 0x00-csharp-hello_world/0-initialize_new_project.sh
@@ -1,4 +1,8 @@
-#!/bin/env bash
+#!/bin/usr/env bash
+
+# Write a Bash script that initializes a new
+# C# project inside a folder titled 0-new_project.
+
 mkdir 0-new_project
 cd 0-new_project
 dotnet new console
---
+++ 0x00-csharp-hello_world/1-build_new_project.sh
@@ -0,0 +1,9 @@
+#!/bin/usr/env bash
+
+# Write a Bash script that initializes and builds
+# a new C# project inside a folder titled 1-new_project
+
+mkdir 1-new_project
+cd 1-new_project
+dotnet new console
+dotnet build
