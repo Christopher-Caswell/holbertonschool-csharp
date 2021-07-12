7661e4a9788693899028dc68509f42a8b97a627d
Christopher-Caswell <christopher.caswell@rocketmail.com>
2021-07-12 17:15:14 -0500

    Init 2

---
+++ 0x00-csharp-hello_world/2-run_new_project.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Write a Bash script that initializes, builds, and
+# runs a new C# project from a folder titled 2-new_project.
+
+mkdir 2-new_project
+cd 2-new_project
+dotnet new console
+dotnet build
+dotnet run
