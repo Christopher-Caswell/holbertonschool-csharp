95e5c3fd7a95579eb573afd6cb000d7b618ddbbc
Christopher-Caswell <christopher.caswell@rocketmail.com>
2021-07-13 17:39:43 -0500

    FizzBuzz

---
+++ 0x01-csharp-ifelse_loops_methods/12-fizzbuzz/12-fizzbuzz.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _12_fizzbuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int woot = 1; woot < 100; woot++)
+            {
+            if (woot % 3 == 0)
+            Console.Write("Fizz");
+            if (woot % 5 == 0)
+            Console.Write("Buzz");
+            if (woot % 3 != 0 && woot % 5 != 0)
+            Console.Write("{0:D}", woot);
+            Console.Write(" ");
+            }
+        Console.WriteLine("Buzz");
+        }
+    }
+}
