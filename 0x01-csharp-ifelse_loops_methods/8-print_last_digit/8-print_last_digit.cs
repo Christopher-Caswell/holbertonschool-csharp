5a11e94841fe1af054e22374f69846dd3b45cdfc
Christopher-Caswell <christopher.caswell@rocketmail.com>
2021-07-13 15:52:54 -0500

    Init 8 loop

---
+++ 0x01-csharp-ifelse_loops_methods/8-print_last_digit/8-print_last_digit.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _8_print_last_digit
+{
+    class Number
+    {
+        public static int PrintLastDigit(int number)
+
+        {
+            Console.Write("{0:D}", Math.Abs(number % 10));
+            return Math.Abs(number % 10);
+        }
+    }
+}
