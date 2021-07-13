b4896632c17b3aea505122c4ddc9d57bfb5c8c82
Christopher-Caswell <christopher.caswell@rocketmail.com>
2021-07-13 16:45:46 -0500

    Init 10 loop

---
+++ 0x01-csharp-ifelse_loops_methods/10-print_line/10-print_line.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Line
+{
+    public static void PrintLine(int length)
+    {
+    for (int woot = 0; woot < length; woot++)
+    {
+        for (int noot = 0; noot < woot; noot++)
+        {
+            Console.Write(" ");
+        }
+        Console.WriteLine("\\");
+    }
+    Console.WriteLine();
+    }
+}
