d953964056208f6c40e6a8580aeed9faef07f7b7
Christopher-Caswell <christopher.caswell@rocketmail.com>
2021-07-13 15:29:50 -0500

    Init 6 loop. Thank you c

--- 0x01-csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
+++ 0x01-csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -6,7 +6,12 @@ namespace _6_print_comb2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            for (int woot = 0; woot < 8; woot++) {
+            for (int noot = woot + 1; noot < 10; noot++) {
+                Console.Write("{0:D}{1:D}, ", woot, noot);
+            }
+        }
+        Console.WriteLine("89");
         }
     }
 }
