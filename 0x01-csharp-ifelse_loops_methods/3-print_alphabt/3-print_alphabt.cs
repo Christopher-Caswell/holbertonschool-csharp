15a9c26504807fea01c32f32ea51084cd7fda47e
Christopher-Caswell <christopher.caswell@rocketmail.com>
2021-07-13 13:43:09 -0500

    Init loop for  alphabet -q,e

--- 0x01-csharp-ifelse_loops_methods/3-print_alphabt/3-print_alphabt.cs
+++ 0x01-csharp-ifelse_loops_methods/3-print_alphabt/3-print_alphabt.cs
@@ -2,11 +2,14 @@
 
 namespace _3_print_alphabt
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+class Program
+{
+static void Main(string[] args)
+{
+char woot;
+for (woot = 'a'; woot <= 'z'; woot++)
+if (woot != 113 && woot != 101)
+Console.Write("{0}", woot);
+}
+}
 }
