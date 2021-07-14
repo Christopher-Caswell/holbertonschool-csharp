389eedb1ea718cb3d5715e60e94fc5f9ab7f3638
Christopher-Caswell <christopher.caswell@rocketmail.com>
2021-07-14 17:58:06 -0500

    Init through 8

---
+++ 0x02-csharp-arrays_lists_dictionaries/6-divisible_by_2/6-divisible_by_2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<bool> DivisibleBy2(List<int> myList)
+    {
+     List<bool> x = new List<bool>();
+
+        foreach (int sel in myList)
+        {
+        x.Add(sel % 2 == 0);
+        }
+        return x;
+    }
+}
---
+++ 0x02-csharp-arrays_lists_dictionaries/8-number_keys/8-number_keys.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+    class Dictionary
+    {
+        public static int NumberOfKeys(Dictionary<string, string> myDict)
+        {
+        int woot = 0;
+        
+        foreach (KeyValuePair<string, string> thing_item_or_whatever in myDict)
+        {
+            woot++;
+        }
+        return woot;
+    }
+    }
