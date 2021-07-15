dd570f5380f27dfe4af1dc24259e916b60925323
Christopher-Caswell <christopher.caswell@rocketmail.com>
2021-07-14 23:16:33 -0500

    Init through 15

---
+++ 0x02-csharp-arrays_lists_dictionaries/12-print_sorted_dictionary/12-print_sorted_dictionary.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static void PrintSorted(Dictionary<string, string> myDict)
+    {
+        List<string> woot = new List<string>(myDict.Keys);
+
+        woot.Sort();
+
+        foreach (string noot in woot)
+        {
+        Console.WriteLine("{0}: {1}", noot, myDict[noot]);
+        }
+    }
+}
---
+++ 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static string BestScore(Dictionary<string, int> myList)
+    {
+        KeyValuePair<string, int> woot = new KeyValuePair<string, int>("None", -1);
+
+        foreach (KeyValuePair<string, int> duo in myList)
+        {
+            if (duo.Value > woot.Value)
+            {
+            woot = duo;
+            }
+        }
+        return woot.Key;
+    }
+}
