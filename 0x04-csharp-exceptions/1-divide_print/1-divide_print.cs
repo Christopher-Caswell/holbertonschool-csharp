a7ea841a9410645d14e1e7c61a09e9202f098636
Christopher-Caswell <christopher.caswell@rocketmail.com>
2021-07-19 07:50:36 -0500

    Init 1

---
+++ 0x04-csharp-exceptions/1-divide_print/1-divide_print.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+class Int
+{
+    public static void divide(int a, int b)
+    {
+    int x = 0;
+    try
+    {
+    x = a / b;
+    }
+    catch
+    {
+    Console.WriteLine("Cannot divide by zero");
+    }
+    finally
+    {
+    Console.WriteLine("{0} / {1} = {2}", a, b, x);
+    }
+    }
+}
