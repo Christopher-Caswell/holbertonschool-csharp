bfcf5f1915cf76895d559728057690557ee08c59
Christopher-Caswell <christopher.caswell@rocketmail.com>
2021-07-20 10:39:36 -0500

    Added a tostring override

--- 0x05-csharp-structs_enums/3-dog/3-dog.cs
+++ 0x05-csharp-structs_enums/3-dog/3-dog.cs
@@ -1,12 +1,40 @@
 ﻿using System;
 
-namespace _3_dog
+class Program
 {
-    class Program
+    public enum Rating
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+        Good,
+        Great,
+        Excellent
+    }
+
+    public struct Dog
+    {
+        public string name;
+        public float age;
+        public string owner;
+        public Rating rating;
+
+    public Dog(string name, float age, string owner, Rating rating)
+    {
+        this.name = name;
+        this.age = age;
+        this.owner = owner;
+        this.rating = rating;
+    }
+    public override string ToString()
+    {
+        return $"Dog Name: {name}\nAge: {age}\nOwner: {owner}\nRating: {rating}";
+    }
+    }
+
+    static void Main(string[] args)
+    {
+        Dog poppy;
+
+        poppy = new Dog("Poppy", 0.7f, "Kris", Rating.Excellent);
+
+        Console.WriteLine(poppy.ToString());
     }
 }
