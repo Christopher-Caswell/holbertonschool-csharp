32d193cdd78f11b5563eb42751a9d0f0dcec60d8
Christopher-Caswell <christopher.caswell@rocketmail.com>
2021-07-20 10:11:08 -0500

    Reup with answers

--- 0x05-csharp-structs_enums/1-dog/1-dog.cs
+++ 0x05-csharp-structs_enums/1-dog/1-dog.cs
@@ -1,12 +1,31 @@
 ﻿using System;
 
-namespace _1_dog
+class Program
 {
-    class Program
+    public enum Rating
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+        Good,
+        Great,
+        Excellent
+    }
+
+    public struct Dog
+    {
+        public string name;
+        public float age;
+        public string owner;
+        public Rating rating;
+    }
+
+    static void Main(string[] args)
+    {
+        Dog poppy;
+
+        poppy.name = "Poppy";
+        poppy.age = 0.7f;
+        poppy.owner = "Kris";
+        poppy.rating = Rating.Excellent;
+
+        Console.WriteLine("My name is {0} and I am {1}. I am an {2} dog. :)", poppy.name, poppy.age, poppy.rating);
     }
 }
