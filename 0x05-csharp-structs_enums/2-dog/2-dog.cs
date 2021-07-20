4727a5d4f0c791035d68586ae5782092d41088c6
Christopher-Caswell <christopher.caswell@rocketmail.com>
2021-07-20 10:35:35 -0500

    Added parameter acceptance

--- 0x05-csharp-structs_enums/2-dog/2-dog.cs
+++ 0x05-csharp-structs_enums/2-dog/2-dog.cs
@@ -1,12 +1,36 @@
 ﻿using System;
 
-namespace _2_dog
+class Program
 {
-    class Program
+    public enum Rating
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+        Good,
+        Great,
+        Excellent
+    }
+
+    public struct Dog
+    {
+        public string name;
+        public float age;
+        public string owner;
+        public Rating rating;
+
+    public Dog(string name, float age, string owner, Rating rating)
+    {
+        this.name = name;
+        this.age = age;
+        this.owner = owner;
+        this.rating = rating;
+    }
+    }
+
+    static void Main(string[] args)
+    {
+        Dog poppy;
+
+        poppy = new Dog("Poppy", 0.7f, "Kris", Rating.Excellent);
+
+        Console.WriteLine("My name is {0} and I am {1}. I am an {2} dog. :)", poppy.name, poppy.age, poppy.rating);
     }
 }
