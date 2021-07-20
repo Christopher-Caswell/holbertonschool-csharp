aec0e3181de6b9a5441f82683f0c1bb23d97ef36
Christopher-Caswell <christopher.caswell@rocketmail.com>
2021-07-20 10:08:01 -0500

    Init bases of files. Up 0

---
+++ 0x05-csharp-structs_enums/0-dog/0-dog.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    public enum Rating
+    {
+        Good,
+        Great,
+        Excellent
+    }
+
+    static void Main(string[] args)
+    {
+        Rating rating;
+
+        rating = Rating.Great;
+
+        Console.WriteLine("Score: " + rating);
+        Console.WriteLine("Score: " + (int)rating);
+    }
+}
---
+++ 0x05-csharp-structs_enums/README.md
@@ -0,0 +1,1 @@
+# A repository for the learning of structs and enums in regards to the Holberton Curriculum
