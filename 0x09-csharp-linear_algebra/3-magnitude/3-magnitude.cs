f1be21abe0c6a62f31ad311830f07a6de3425c62
Christopher-Caswell <christopher.caswell@rocketmail.com>
2021-07-28 02:06:08 -0500

    Create repo, added most text files

---
+++ 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+/// <summary>
+/// The magnitude of a vector.
+/// </summary>
+class VectorMath
+{
+    /// <summary>
+    /// Calculate the magnitude of a vector. 2D and 3D vectors are supported.
+    /// </summary>
+    public static double Magnitude(double[] vector)
+    {
+        if (vector.Length == 2)
+        {
+            return Math.Round(Math.Sqrt(vector[0] * vector[0] + vector[1] * vector[1]), 2);
+        }
+        else if (vector.Length == 3)
+        {
+            return Math.Round(Math.Sqrt(vector[0] * vector[0] + vector[1] * vector[1] + vector[2] * vector[2]), 2);
+        }
+        return -1;
+    }
+}
---
+++ 0x09-csharp-linear_algebra/README.md
@@ -0,0 +1,1 @@
+# Welcome back to high school, now with extra propgramming! Curriculum for Holberton
