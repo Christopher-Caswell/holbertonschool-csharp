acc049336d7b056b90423920905897d2fd2540ac
Christopher-Caswell <christopher.caswell@rocketmail.com>
2021-08-02 22:58:12 -0500

    Init 9

---
+++ 0x00-csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+/// <summary>
+/// Vector math.
+/// </summary>
+class VectorMath
+{   
+    /// <summary>
+    /// Vector scalar multiplication.
+    /// </summary>
+    public static double[] Multiply(double[] vector, double scalar)
+    {
+        if (vector.Length == 3)
+        {
+            return new double[] { vector[0] * scalar, vector[1] * scalar, vector[2] * scalar };
+        }
+        if (vector.Length == 2)
+        {
+            return new double[] { vector[0] * scalar, vector[1] * scalar };
+        }
+        return new double[] {-1};
+    }
+}
---
+++ 0x00-csharp-linear_algebra/9-vector_scalar_mul/main.cs
@@ -0,0 +1,10 @@
+using System;
+
+class VectorScalarMul
+{
+    static void Main()
+    {
+       x = Multiply(new double[] { 1, 2, 3 }, 2);
+       Console.WriteLine(x);
+    }
+}
